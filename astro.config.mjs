729dfe4b35ac254d23e162c419364512a3190822
Walter Plaza <shuke656@gmail.com>
2025-05-08 20:31:43 -0300

    Añadiendo seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    }, 
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: "https://www.digitaladvisors.com.ar",
+  integrations: [sitemap()],
 });
